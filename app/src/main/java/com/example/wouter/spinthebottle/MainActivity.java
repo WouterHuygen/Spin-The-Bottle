6d3edf655351d67403b29d3dd14d9decf72b007a
Unknown <cristian.baba09@yahoo.com>
2017-12-13 13:13:46 +0100

    Layout Mooi gemaakt + Buttons naar juiste activity

--- app/src/main/java/com/example/wouter/spinthebottle/MainActivity.java
+++ app/src/main/java/com/example/wouter/spinthebottle/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wouter.spinthebottle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Halloooo
-        //dag mn jonge
-        //hoe gaat het
+        // playbutton om naar  GameActivity te navigeren
+        Button playbtn = (Button) findViewById(R.id.btnPlay);
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+        //button om naar  Setting te navigeren
+        Button settingsbtn = (Button) findViewById(R.id.btnSettings);
+        settingsbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
