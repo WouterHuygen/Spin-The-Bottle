1c4004ca32680ababbbfe52247fe6fe34579f2e1
Unknown <cristian.baba09@yahoo.com>
2017-12-17 22:21:36 +0100

    Gyro geadd voor bottle te laten spinnen WIP

--- app/src/main/java/com/example/wouter/spinthebottle/GameActivity.java
+++ app/src/main/java/com/example/wouter/spinthebottle/GameActivity.java
@@ -1,34 +1,72 @@
 package com.example.wouter.spinthebottle;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class GameActivity extends AppCompatActivity {
+import java.lang.reflect.Type;
+import java.util.Random;
 
+public class GameActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor Accelero;
+    private SensorManager SensorM;
+    private static final String Tag = "MainActivity";
+    private static int currentstate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        //Sensormeter + accelerometer aanmaken
+       SensorM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Accelero = SensorM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorM.registerListener(this,Accelero,SensorManager.SENSOR_DELAY_GAME);
+
+
+
+
+        // Gameplay Class
+       final Gameplay newgame = new Gameplay();
+
+
+        // Initializatie van buttons imageview enz..
         final ImageView Bottle = (ImageView) findViewById(R.id.bottlespinImg);
         Button playGameBtn = (Button) findViewById(R.id.gameplaybtn);
 
+        // Button Click Listner voor de game
         playGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RotateAnimation bottlerotation = new RotateAnimation(0,360,
-                        RotateAnimation.RELATIVE_TO_SELF,
-                        0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
-                bottlerotation.setFillAfter(true);
-                bottlerotation.setDuration(1000);
+                //rotatie testen
+                System.out.println(newgame.getrotation());
+                newgame.Rotateanimation(Bottle);
 
-                Bottle.startAnimation(bottlerotation);
             }
         });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(Tag, "OnSensorChanged: X :" + sensorEvent.values[0] + "Y:" + sensorEvent.values[1] + "Z:" + sensorEvent.values[2]);
+
+        sensorEvent.values[1] = currentstate;
+        if(sensorEvent.values[1] != currentstate) {
+           // newgame.Rotateanimation(Bottle);
+        }
     }
 }
--- app/src/main/java/com/example/wouter/spinthebottle/Gameplay.java
+++ app/src/main/java/com/example/wouter/spinthebottle/Gameplay.java
@@ -1,8 +1,38 @@
 package com.example.wouter.spinthebottle;
 
+import android.content.Context;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import java.util.Random;
+
 /**
  * Created by crist on 17-12-2017.
  */
 
 public class Gameplay {
-}
+
+
+    public Gameplay() {
+
+    }
+    private static Random r = new Random();
+    private int rotation = 360;
+
+
+    public int getrotation() {
+        this.rotation+= (r.nextInt(360)+1080)-60;
+        if(rotation > 3000)
+            rotation = 360+(r.nextInt(360)+720)/2;
+        return rotation;
+    }
+    public void Rotateanimation (ImageView WhattoSpin){
+        RotateAnimation bottlerotation = new RotateAnimation(0,rotation,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        bottlerotation.setFillAfter(true);
+        bottlerotation.setDuration(1000);
+        WhattoSpin.startAnimation(bottlerotation);
+
+    }
+    }
+
