3c2d9668131222648c63a8443fb660dda517daa8
Skarner <lemmanavenra@gmail.com>
2017-12-20 11:33:27 +0100

    Classes aangemaakt+comments in layouts

---
+++ app/src/main/java/com/example/wouter/spinthebottle/GameQuestions.java
@@ -0,0 +1,15 @@
+package com.example.wouter.spinthebottle;
+
+/**
+ * Created by lemma on 20/12/2017.
+ */
+
+public class GameQuestions {
+   /* public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }*/
+}
---
+++ app/src/main/java/com/example/wouter/spinthebottle/GameQuestionsList.java
@@ -0,0 +1,9 @@
+package com.example.wouter.spinthebottle;
+
+/**
+ * Created by lemma on 20/12/2017.
+ */
+
+public class GameQuestionsList {
+
+}
